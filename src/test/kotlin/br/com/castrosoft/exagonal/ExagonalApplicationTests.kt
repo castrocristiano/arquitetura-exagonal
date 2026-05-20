83ea507dbd073ceaf8c55254f93f053041fe61b5
Cristiano Castro <4948530+castrocristiano@users.noreply.github.com>
2026-05-19 22:44:39 -0300

    test

--- src/test/kotlin/br/com/castrosoft/exagonal/ExagonalApplicationTests.kt
+++ src/test/kotlin/br/com/castrosoft/exagonal/ExagonalApplicationTests.kt
@@ -1,13 +1,12 @@
 package br.com.castrosoft.exagonal
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class ExagonalApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		println("teste")
 	}
 
 }
