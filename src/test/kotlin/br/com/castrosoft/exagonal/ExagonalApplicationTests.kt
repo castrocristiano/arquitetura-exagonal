c6bf44ff652a8e0e321514ef7efdf3d8543ec6ff
Cristiano Castro <4948530+castrocristiano@users.noreply.github.com>
2026-05-19 23:00:00 -0300

    test

--- src/test/kotlin/br/com/castrosoft/exagonal/ExagonalApplicationTests.kt
+++ src/test/kotlin/br/com/castrosoft/exagonal/ExagonalApplicationTests.kt
@@ -1,12 +1,14 @@
 package br.com.castrosoft.exagonal
 
 import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
 
 class ExagonalApplicationTests {
 
 	@Test
 	fun contextLoads() {
 		println("teste")
+		assertTrue { true }
 	}
 
 }
